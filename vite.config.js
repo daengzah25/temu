2bcef2354556e8735487ebb16df0d4fc1f2a6cd6
Daffa Fadillah <daffaakbarfadillah@MacBook-Air-Dafong.local>
2025-11-28 16:18:16 +0700

    railway.json

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
 });
